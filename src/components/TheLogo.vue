6a88c23182f37777f6bec69b3d22d38a0463eaf3
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-02-15 18:38:02 -0300

    fix: arruma posicionamento do icone da Logo

--- src/components/TheLogo.vue
+++ src/components/TheLogo.vue
@@ -4,6 +4,15 @@
 
 <template>
 	<RouterLink :to="{ path: '/' }">
-		<i><PhGlobeHemisphereWest color="#9740EF" :size="36" /></i>
+		<i class="logo__icon"
+			><PhGlobeHemisphereWest color="#9740EF" :size="36"
+		/></i>
 	</RouterLink>
 </template>
+
+<style lang="scss" scoped>
+	@import "../styles/scss/main";
+	.logo__icon {
+		@include center(row);
+	}
+</style>
