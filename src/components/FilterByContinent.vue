639dc53f2079697ee462fcb217828f4e37d6a8fa
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-02-17 16:47:02 -0300

    feat: adiciona componente FilterByContinent

---
+++ src/components/FilterByContinent.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+	import { ref, watch } from "vue";
+	import BaseSelect, { OptionProps } from "./BaseSelect.vue";
+
+	type FilterByContinentProps = {
+		handleChange: (selectedContinent: string) => void;
+	};
+
+	const props = defineProps<FilterByContinentProps>();
+	const selectedContinent = ref("");
+
+	const options: OptionProps[] = [
+		{ text: "Todos", value: "" },
+		{ text: "África", value: "africa" },
+		{ text: "América", value: "america" },
+		{ text: "Asia", value: "asia" },
+		{ text: "Europa", value: "europe" },
+		{ text: "Oceania", value: "oceania" },
+	];
+
+	watch(selectedContinent, () => props.handleChange(selectedContinent.value));
+</script>
+
+<template>
+	<BaseSelect
+		v-model="selectedContinent"
+		label="Filtrar por continente"
+		:options="options"
+	/>
+</template>
+
+<style scoped></style>
