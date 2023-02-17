6cbfe05516b3231622cb7e68701b0cb0b94ec112
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-02-17 17:32:41 -0300

    feat: adiciona estrutura estática da página Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,41 @@
+<script lang="ts" setup>
+	import FilterByContinent from "../components/FilterByContinent.vue";
+	import SearchBar from "../components/SearchBar.vue";
+
+	import DefaultLayout from "../layouts/DefaultLayout.vue";
+</script>
+
 <template>
-	<h1>Hello World H1</h1>
+	<DefaultLayout>
+		<div class="home-page">
+			<h1 class="home-page__title">Explore os países do mundo!</h1>
+			<section class="filters">
+				<SearchBar :handleChange="(v) => {}" />
+				<FilterByContinent :handleChange="(v) => {}" />
+			</section>
+			<article class="countries"></article>
+		</div>
+	</DefaultLayout>
 </template>
+
+<style scoped lang="scss">
+	@import "../styles/scss/main";
+
+	.home-page {
+		width: 100%;
+	}
+	.home-page__title {
+		max-width: 290px;
+		padding-top: 32px;
+	}
+
+	.filters {
+		@include center(row);
+		justify-content: space-between;
+		gap: 12px;
+		flex-wrap: wrap;
+		padding: 48px 0px;
+	}
+	.coutries {
+	}
+</style>
