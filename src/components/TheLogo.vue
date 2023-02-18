bbf508c013005644ff20f80cc8cd65efb468d2b0
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-02-18 16:40:39 -0300

    refactor: melhora a reusabilidade do ícone do componente TheLogo

--- src/components/TheLogo.vue
+++ src/components/TheLogo.vue
@@ -1,18 +1,10 @@
 <script lang="ts" setup>
 	import { RouterLink } from "vue-router";
+	import LogoIcon from "./LogoIcon.vue";
 </script>
 
 <template>
 	<RouterLink :to="{ path: '/' }">
-		<i class="logo__icon"
-			><PhGlobeHemisphereWest color="#9740EF" :size="36"
-		/></i>
+		<LogoIcon />
 	</RouterLink>
 </template>
-
-<style lang="scss" scoped>
-	@import "../styles/scss/main";
-	.logo__icon {
-		@include center(row);
-	}
-</style>
