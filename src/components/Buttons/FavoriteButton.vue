ea46355ebb120397837795ffc5aaf09d704987eb
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-02-18 17:54:42 -0300

    feat: adiciona componente FavoriteButton

---
+++ src/components/Buttons/FavoriteButton.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+	import { PhHeart } from "phosphor-vue";
+	import { ButtonHTMLAttributes } from "vue";
+
+	interface FavoriteButtonProps extends Omit<ButtonHTMLAttributes, "type"> {
+		isFavorite: boolean;
+	}
+
+	const props = defineProps<FavoriteButtonProps>();
+</script>
+
+<template>
+	<button
+		type="button"
+		title="Favoritar"
+		class="favorite-button"
+		v-bind="$attrs"
+	>
+		<i class="favorite-button__icon"
+			><PhHeart
+				:size="24"
+				:weight="props.isFavorite ? 'duotone' : 'regular'"
+				:color="props.isFavorite ? '#9740EF' : '#f2f2f2'"
+		/></i>
+	</button>
+</template>
+
+<style scoped lang="scss">
+	@import "../../styles/scss/main";
+
+	.favorite-button {
+		padding: 8px;
+		background: none;
+		border: none;
+		cursor: pointer;
+
+		&:hover {
+			background-color: #f2f2f21d;
+			border-radius: 100%;
+		}
+	}
+	.favorite-button__icon {
+		@include center(row);
+	}
+</style>
