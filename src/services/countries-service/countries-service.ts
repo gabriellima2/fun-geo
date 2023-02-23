e05a6d9cd6314b10b3a858fe372bdb988999f44d
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-02-23 14:59:20 -0300

    style: apaga logs

--- src/services/countries-service/countries-service.ts
+++ src/services/countries-service/countries-service.ts
@@ -14,11 +14,7 @@ export const countriesService = {
 		const data: T = await response.json();
 		return data;
 	},
-	getByName: async <T extends object | []>(
-		name: string,
-		fields = `${FIELDS},currencies,capital,continents,languages,area,population`
-	) => {
-		console.log(name);
+	getByName: async <T extends object | []>(name: string, fields = FIELDS) => {
 		if (!name) throw new Error("Please, pass valid name parameter");
 		if (!fields) throw new Error(EMPTY_PARAMETER_ERROR_MESSAGE);
 
@@ -26,7 +22,6 @@ export const countriesService = {
 		if (!response.ok) throw new Error(response.statusText);
 
 		const data: T = await response.json();
-		console.log(data);
 		return data;
 	},
 };
