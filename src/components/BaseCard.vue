68f6b1083760520977a6a64de4e32f49cd6d25c0
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-02-24 18:15:45 -0300

    refactor: melhora reusabilidade de estilos

---
+++ src/components/BaseCard.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+	import { HTMLAttributes } from "vue";
+
+	interface BaseCardProps extends HTMLAttributes {}
+	const props = defineProps<BaseCardProps>();
+</script>
+
+<template>
+	<article class="base-card" v-bind="props">
+		<header class="base-card__header">
+			<slot name="base-card-header"></slot>
+		</header>
+		<section class="base-card__body">
+			<slot name="base-card-body"></slot>
+		</section>
+	</article>
+</template>
+
+<style scoped lang="scss">
+	@import "../styles/scss/main";
+
+	$border-default-style: 1px solid #242424;
+	$border-radius: 8px;
+
+	.base-card {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		width: 100%;
+	}
+	.base-card__header {
+		border-radius: $border-radius;
+		border: $border-default-style;
+		padding: 8px;
+	}
+
+	.base-card__body {
+		padding: 8px;
+		flex: 1;
+		border-radius: $border-radius;
+		border: $border-default-style;
+		background-color: $util-color-900;
+	}
+</style>
--- src/components/Countries/CountriesOverviewListItem.vue
+++ src/components/Countries/CountriesOverviewListItem.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 	import { useFavoriteCountries } from "./composables/useFavoriteCountries";
+
 	import FavoriteButton from "../Buttons/FavoriteButton.vue";
+	import BaseCard from "../BaseCard.vue";
 
 	type CountriesOverviewListItemProps = {
 		id: string;
@@ -17,25 +19,31 @@
 </script>
 
 <template>
-	<li class="country-list-item">
+	<li class="overview-item">
 		<RouterLink :to="{ path: `detalhes-nacao/${props.id}` }" class="country">
-			<div class="country__image-container">
-				<img
-					:src="props.flag.src"
-					:alt="props.flag.alt"
-					class="country__image"
-				/>
-			</div>
-			<section class="preview">
-				<div class="texts">
-					<h1 class="texts__title">{{ props.name }}</h1>
-					<h2 class="texts__subtitle">{{ props.region }}</h2>
-				</div>
-				<FavoriteButton
-					@click.prevent="() => handleFavorite(props.id)"
-					:is-favorite="false"
-				/>
-			</section>
+			<BaseCard class="overview-item">
+				<template #base-card-header>
+					<div class="country__image-container">
+						<img
+							:src="props.flag.src"
+							:alt="props.flag.alt"
+							class="country__image"
+						/>
+					</div>
+				</template>
+				<template #base-card-body>
+					<div class="preview">
+						<div class="texts">
+							<h1 class="texts__title">{{ props.name }}</h1>
+							<h2 class="texts__subtitle">{{ props.region }}</h2>
+						</div>
+						<FavoriteButton
+							@click.prevent="() => handleFavorite(props.id)"
+							:is-favorite="false"
+						/>
+					</div>
+				</template>
+			</BaseCard>
 		</RouterLink>
 	</li>
 </template>
@@ -45,7 +53,7 @@
 
 	$border-default-style: 1px solid #242424;
 	$border-radius: 8px;
-	.country-list-item {
+	.overview-item {
 		width: 100%;
 		height: 100%;
 	}
@@ -54,25 +62,20 @@
 		display: flex;
 		flex-direction: row;
 
-		&:hover > .preview,
-		&:focus > .preview {
+		&:hover,
+		&:focus {
 			transition: filter 0.1s ease-in-out;
 			filter: brightness(1.3);
 		}
 
-		&:hover > .country__image-container,
-		&:focus > .country__image-container {
+		&:hover > .country__image,
+		&:focus > .country__image {
 			transition: background-color 0.1s ease-in-out;
 			background-color: $util-color-900;
 		}
 	}
 	.country__image-container {
 		@include center(row);
-		border-top-left-radius: $border-radius;
-		border-bottom-left-radius: $border-radius;
-		border: $border-default-style;
-		border-right: 0;
-		padding: 8px;
 	}
 	.country__image {
 		width: 110px;
@@ -80,15 +83,11 @@
 		border-radius: 6px;
 	}
 	.preview {
+		height: 100%;
 		display: flex;
 		justify-content: space-between;
 		align-items: flex-start;
 		flex: 1;
-		padding: 4px 4px 4px 16px;
-		border-top-right-radius: $border-radius;
-		border-bottom-right-radius: $border-radius;
-		border: $border-default-style;
-		background-color: $util-color-900;
 	}
 	.texts {
 		@include center(column);
@@ -102,8 +101,6 @@
 		max-height: 100%;
 	}
 	.texts__subtitle {
-		font-size: 0.8rem;
-		font-weight: 500;
-		opacity: 0.6;
+		@include subtitle(0.8rem);
 	}
 </style>
