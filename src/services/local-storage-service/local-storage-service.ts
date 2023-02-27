5eb9cd5c36bcac25bc9b958c884436a6f00fd76d
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-02-27 17:55:11 -0300

    fix: arruma método set do local-storage-service

--- src/services/local-storage-service/local-storage-service.ts
+++ src/services/local-storage-service/local-storage-service.ts
@@ -5,10 +5,7 @@ type LocalStorageServiceParams<T> = {
 
 class LocalStorageService {
 	set<T>({ key, value }: LocalStorageServiceParams<T>) {
-		window.localStorage.setLocalStorageServiceParams(
-			key,
-			JSON.stringify(value)
-		);
+		window.localStorage.setItem(key, JSON.stringify(value));
 	}
 	get<T>({ key }: Pick<LocalStorageServiceParams<T>, "key">): T | null {
 		const item = window.localStorage.getItem(key);
