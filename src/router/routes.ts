e7cffbd927eaf1c30a801f29f2e67496d5226983
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-03-02 17:45:39 -0300

    feat: adiciona rota /praticar

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,6 +4,7 @@ import Home from "@/views/Home.vue";
 const CountryDetails = () => import("../views/CountryDetails.vue");
 const SearchResult = () => import("../views/SearchResult.vue");
 const Favorites = () => import("../views/Favorites.vue");
+const Practice = () => import("../views/Practice.vue");
 
 export const routes: RouteRecordRaw[] = [
 	{ path: "/", name: "Home", component: Home },
@@ -14,4 +15,5 @@ export const routes: RouteRecordRaw[] = [
 	},
 	{ path: "/resultados", name: "SearchResult", component: SearchResult },
 	{ path: "/favoritos", name: "Favorites", component: Favorites },
+	{ path: "/praticar", name: "Practice", component: Practice },
 ];
